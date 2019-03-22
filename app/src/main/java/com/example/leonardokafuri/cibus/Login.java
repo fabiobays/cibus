20e25147f233f51898ea2fec7ddbb9ba46c20187
leonardokafuri <kafurileo@gmail.com>
2019-03-21 18:29:11 -0700

    Updated layout again and add more fields

--- app/src/main/java/com/example/leonardokafuri/cibus/Login.java
+++ app/src/main/java/com/example/leonardokafuri/cibus/Login.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,7 +21,7 @@ public class Login extends AppCompatActivity {
         Button Forgot = findViewById(R.id.forgot);
         final Button Login = findViewById(R.id.login);
         final EditText userName = findViewById(R.id.username);
-        final EditText password = findViewById(R.id.password);
+        final EditText password = findViewById(R.id.CardNum);
         Intent i = getIntent();
         String value = i.getStringExtra("registered");
         if(value != null && !value.isEmpty())
